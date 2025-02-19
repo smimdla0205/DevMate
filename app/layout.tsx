3f212c4a913200136dadbcc56a3c1bdcb9b79538
smimdla0205 <sangmin0205@gmail.com>
2025-02-19 16:41:52 +0900

    ESLint 및 Prettier 설정 추가, 불필요한 ESLint 구성 파일 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
-import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Inter } from "next/font/google";
+
 import type { ReactNode } from "react";
-import {Inter} from "next/font/google";
-import localFont from 'next/font/local';
+
+import type { Metadata } from "next";
+
 import "@/styles/globals.scss";
 
 export const metadata: Metadata = {
@@ -15,24 +18,16 @@ const pretendard = localFont({
   weight: "45 920",
 });
 
-
-
 const inter = Inter({
-  subsets: ['latin'], // 라틴 문자 집합만 로드
-  weight: ["300",'400','500', '600', '700'],  // 사용할 폰트 두께
-  display: 'swap', // 폰트가 로딩되면 즉시 텍스트를 표시하고, 로딩이 완료되면 폰트를 교체하는 방식
+  subsets: ["latin"], // 라틴 문자 집합만 로드
+  weight: ["300", "400", "500", "600", "700"], // 사용할 폰트 두께
+  display: "swap", // 폰트가 로딩되면 즉시 텍스트를 표시하고, 로딩이 완료되면 폰트를 교체하는 방식
 });
 
-export default function RootLayout({
-  children,
-}:{
-  children: ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
-      <body className={`${pretendard.className}${inter.className}`}>
-        {children}
-      </body>
+      <body className={`${pretendard.className}${inter.className}`}>{children}</body>
     </html>
   );
 }
