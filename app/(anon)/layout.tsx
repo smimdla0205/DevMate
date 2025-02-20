21fec1e6116e376fc674316af455cc45ca4821b5
황혜인 <119389723+hwanghaein@users.noreply.github.com>
2025-02-20 17:02:29 +0900

    feat : 공통 컴포넌트 제작 - footer #5

--- app/(anon)/layout.tsx
+++ app/(anon)/layout.tsx
@@ -1,15 +1,20 @@
 import type { ReactNode } from "react";
 
 import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
+
+import styles from "./layout.module.scss";
 
 export default function AnonLayout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <Header />
-      <div>
-        <main style={{ paddingTop: "64px" }}>{children}</main>
-      </div>
-      <footer>푸터</footer>
-    </>
+    <div className={styles.layout}>
+      <header>
+        <Header />
+      </header>
+      <main className={styles.main}>{children}</main> {/* main에 클래스 추가 */}
+      <footer className={styles.footer}>
+        <Footer />
+      </footer>
+    </div>
   );
 }
