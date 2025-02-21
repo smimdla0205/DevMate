eb0bf26acc4210d7a2204b741aa2166aa911c40a
yezzero <yeyeonggim06@gmail.com>
2025-02-21 10:18:32 +0900

    refactor: classname도 props로 받도록 수정

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,17 +1,27 @@
+import type { ButtonHTMLAttributes } from "react";
+
 import styles from "./button.module.scss";
 
 type buttonVariant = "main" | "sub" | "black" | "outline";
 type buttonSize = "small" | "large" | "long";
 
-interface ButtonProps extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "onClick"> {
+interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "onClick"> {
   variant?: buttonVariant;
   size?: buttonSize;
-  onClick: () => void; // ✅ 필수 속성으로 지정
+  onClick: () => void;
 }
 
-export default function Button({ children, variant = "main", size = "small", onClick, ...props }: ButtonProps) {
-  // eslint-disable-next-line security/detect-object-injection
-  const buttonClass = `${styles.button} ${variant ? styles[variant] : "main"} ${size ? styles[size] : "small"}`.trim();
+export default function Button({
+  children,
+  variant = "main",
+  size = "small",
+  onClick,
+  className = "",
+  ...props
+}: ButtonProps) {
+  const buttonClass =
+    // eslint-disable-next-line security/detect-object-injection
+    `${styles.button} ${variant ? styles[variant] : "main"} ${size ? styles[size] : "small"} ${className}`.trim();
 
   return (
     <button type="button" className={buttonClass} onClick={onClick} {...props}>
