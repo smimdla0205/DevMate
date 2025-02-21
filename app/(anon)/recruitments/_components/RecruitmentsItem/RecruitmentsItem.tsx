5efe20655d1acea6d3b259c64e923d068de4bc05
황혜인 <119389723+hwanghaein@users.noreply.github.com>
2025-02-21 16:57:05 +0900

    feat: 메인 페이지에서 태그 컴포넌트 분리

--- app/(anon)/recruitments/_components/RecruitmentsItem/RecruitmentsItem.tsx
+++ app/(anon)/recruitments/_components/RecruitmentsItem/RecruitmentsItem.tsx
@@ -1,5 +1,7 @@
 import styles from "./RecruitmentsItem.module.scss";
 
+import RecruitmentsTag from "../RecruitmentsTag/RecruitmentsTag";
+
 import { FaHeart, FaEye, FaComment } from "react-icons/fa";
 
 export default function RecruitmentsItem() {
@@ -12,10 +14,7 @@ export default function RecruitmentsItem() {
             <h2 className={styles["main__post-title"]}>프로젝트 제목</h2>
           </div>
           <p className={styles["main__post-description"]}>프로젝트 설명이 여기에 들어갑니다.</p>
-          <div className={styles["main__post-tags"]}>
-            <span className={styles["main__post-tag"]}>#React</span>
-            <span className={styles["main__post-tag"]}>#Next.js</span>
-          </div>
+          <RecruitmentsTag tags={["Next.js", "React"]} />
           <div className={styles["main__post-meta"]}>
             <div>
               <span className={styles["main__post-author"]}>신짱구</span>
