89640ffcace0fc54e225db880d072295f00d089b
yezzero <yeyeonggim06@gmail.com>
2025-02-24 16:31:23 +0900

    fix: 멤버 table에 수락된 멤버만 뜨도록 수정

--- app/user/mypage/projects/[id]/_components/membersSection.tsx
+++ app/user/mypage/projects/[id]/_components/membersSection.tsx
@@ -1,16 +1,18 @@
 "use client";
 
-import Table from "@/components/table/Table";
+import Table from "@/components/table/table";
 
 import styles from "../ProjectDetail.module.scss";
 
 import type { Applicant } from "./projectData";
 
 export default function MembersSection({ applications }: { applications: Applicant[] }) {
-  const transformedMembers = applications.map((app) => ({
-    ...app,
-    user: typeof app.user === "object" ? app.user.name : app.user,
-  }));
+  const transformedMembers = applications
+    .map((app) => ({
+      ...app,
+      user: typeof app.user === "object" ? app.user.name : app.user,
+    }))
+    .filter((app) => app.status === "accept");
 
   return (
     <div className={styles.container__content} style={{ width: "50%" }}>
